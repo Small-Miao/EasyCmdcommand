113428f76c6d2789427aed1d2281cbd5a9abf616
烦 哈哈 <1773805744@qq.com>
2018-06-08 09:51:00 +0800

    EasyCmdcommand Finsih

---
+++ EasyCmdCommand/Class1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+
+namespace EasyCmdCommand
+{
+    public class Class1
+    {
+        public void cmdcommand(string cmdcommand)
+        {
+            Process p = new Process();
+            p.StartInfo.FileName = "cmd.exe";
+            p.StartInfo.UseShellExecute = false;
+            p.StartInfo.RedirectStandardInput = true;
+            p.StartInfo.RedirectStandardOutput = true;
+            p.StartInfo.RedirectStandardError = true;
+            p.StartInfo.CreateNoWindow = true;
+            p.Start();
+            p.StandardInput.WriteLine(cmdcommand + "&exit");
+            p.StandardInput.AutoFlush = true;
+            string strOuput = p.StandardOutput.ReadToEnd();
+            p.WaitForExit();
+            p.Close();
+            Console.WriteLine(strOuput);
+        }
+    }
+}
